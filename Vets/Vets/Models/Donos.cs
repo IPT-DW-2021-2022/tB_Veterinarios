6879f8870745a86d9a28b4f4e4538d5a7fc3a67b
jcnpereira <casimiro@ipt.pt>
2022-03-29 17:22:28 +0100

    adição de anotações para condicionar o comportamento do Model

--- Vets/Vets/Models/Donos.cs
+++ Vets/Vets/Models/Donos.cs
@@ -1,19 +1,42 @@
-﻿namespace Vets.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vets.Models {
+
+   /// <summary>
+   /// Representa os dados do Dono de um Animal
+   /// </summary>
    public class Donos {
 
       public Donos() {
          ListaAnimais = new HashSet<Animais>();
       }
 
+      /// <summary>
+      /// PK para a tabela dos Donos
+      /// </summary>
       public int Id { get; set; }
 
+      /// <summary>
+      /// Nome do Dono do animal
+      /// </summary>
+     [Required(ErrorMessage ="O {0} é de preenchimento obrigatório")]
       public string Nome { get; set; }
 
+      /// <summary>
+      /// NIF do dono do animal
+      /// </summary>
+      [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
       public string NIF { get; set; }
 
+      /// <summary>
+      /// sexo do dono 
+      /// Ff - feminino; Mm - masculino
+      /// </summary>
       public string Sexo { get; set; }
 
-
+      /// <summary>
+      /// Lista dos animais de quem o Dono é dono
+      /// </summary>
       public ICollection<Animais> ListaAnimais { get; set; }
 
    }
