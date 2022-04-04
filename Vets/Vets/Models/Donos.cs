e06dec65e8ad6230ed998243be3ea87e03ee040f
jcnpereira <casimiro@ipt.pt>
2022-04-04 17:23:52 +0100

    adição de anotadores: StringLenght + RegularExpression

--- Vets/Vets/Models/Donos.cs
+++ Vets/Vets/Models/Donos.cs
@@ -19,19 +19,26 @@ namespace Vets.Models {
       /// <summary>
       /// Nome do Dono do animal
       /// </summary>
-     [Required(ErrorMessage ="O {0} é de preenchimento obrigatório")]
+      [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+      [StringLength(30, ErrorMessage = "O {0} não pode ter mais do que {1} caracteres.")]
+      [RegularExpression("[A-ZÂÓÍa-záéíóúàèìòùâêîôûãõäëïöüñç '-]+",
+                          ErrorMessage ="No {0} só são aceites letras")]
       public string Nome { get; set; }
 
       /// <summary>
       /// NIF do dono do animal
       /// </summary>
       [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+      [StringLength(9, MinimumLength = 9, ErrorMessage = "O {0} deve ter exatamente {1} caracteres.")]
+      [RegularExpression("[123578]+[0-9]{8}", ErrorMessage = "O {0} deve começar por 1, 2, 3, 5, 7 ou 8, e só ter algarismos.")]
       public string NIF { get; set; }
 
       /// <summary>
       /// sexo do dono 
       /// Ff - feminino; Mm - masculino
       /// </summary>
+      [StringLength(1, ErrorMessage = "O {0} só aceita um caráter.")]
+      [RegularExpression("[FfMm]", ErrorMessage = "No {0} só se aceitam as letras F ou M.")]
       public string Sexo { get; set; }
 
       /// <summary>
