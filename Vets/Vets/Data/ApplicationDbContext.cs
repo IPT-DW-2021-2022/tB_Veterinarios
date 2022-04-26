8990ee6dc73c6ab2ca6c2384fd780277f928cd43
jcnpereira <casimiro@ipt.pt>
2022-04-26 16:30:51 +0100

    adicionar Seed à BD

--- Vets/Vets/Data/ApplicationDbContext.cs
+++ Vets/Vets/Data/ApplicationDbContext.cs
@@ -14,6 +14,46 @@ namespace Vets.Data {
       }
 
 
+      /// <summary>
+      /// este método é executado imediatamente antes 
+      /// da criação do Modelo.
+      /// É utilizado para adicionar as últimas instruções
+      /// à criação do modelo
+      /// </summary>
+      /// <param name="modelBuilder"></param>
+      protected override void OnModelCreating(ModelBuilder modelBuilder) {
+         // 'importa' todo o comportamento do método, 
+         // aquando da sua definição na SuperClasse
+         base.OnModelCreating(modelBuilder);
+
+         // adicionar registos que serão adicionados às
+         // tabelas da BD
+         modelBuilder.Entity<Veterinarios>().HasData(
+            new Veterinarios() {
+               Id = 1,
+               Nome = "José Silva",
+               NumCedulaProf = "vet-8765",
+               Fotografia = "Jose.jpg"
+            },
+            new Veterinarios() {
+               Id = 2,
+               Nome = "Maria Gomes dos Santos",
+               NumCedulaProf = "vet-6542",
+               Fotografia = "Maria.jpg"
+            },
+            new Veterinarios() {
+               Id = 3,
+               Nome = "Ricardo Sousa",
+               NumCedulaProf = "vet-1339",
+               Fotografia = "Ricardo.jpg"
+            }
+         );
+
+      }
+
+
+
+
       // definir as 'tabelas'
       public DbSet<Animais> Animais { get; set; }
       public DbSet<Veterinarios> Veterinarios { get; set; }
