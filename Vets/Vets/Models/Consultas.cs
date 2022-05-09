dbe53d4eaaec7694a33f5d9edd2fc452c9f580a8
jcnpereira <casimiro@ipt.pt>
2022-05-09 16:21:28 +0100

    formatação da data

--- Vets/Vets/Models/Consultas.cs
+++ Vets/Vets/Models/Consultas.cs
@@ -1,10 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Vets.Models {
    public class Consultas {
 
       public int Id { get; set; }
 
+      [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",
+                     ApplyFormatInEditMode = true)]
+      [DataType(DataType.Date)]
       public DateTime Data { get; set; }
 
       public string Observacoes { get; set; }
@@ -17,7 +21,7 @@ namespace Vets.Models {
       public Animais Animal { get; set; }
 
 
-      [ForeignKey(nameof (Veterinario))]
+      [ForeignKey(nameof(Veterinario))]
       public int VeterinarioFK { get; set; }
       public Veterinarios Veterinario { get; set; }
 
