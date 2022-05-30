8785f8aad20b95deb77a1de4429c0209568d1744
jcnpereira <casimiro@ipt.pt>
2022-05-30 16:52:36 +0100

    configurações finais

--- Vets/Vets/Program.cs
+++ Vets/Vets/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,11 +27,46 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(   // builder.Services.AddD
 /* declarar o servi�o das vars de sess�o */
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession(options => {
-      options.IdleTimeout = TimeSpan.FromSeconds(120);
-      options.Cookie.HttpOnly = true;
-      options.Cookie.IsEssential = true;
-   }
-   );
+   options.IdleTimeout = TimeSpan.FromSeconds(120);
+   options.Cookie.HttpOnly = true;
+   options.Cookie.IsEssential = true;
+});
+
+
+// https://docs.microsoft.com/en-us/aspnet/core/security/authentication/identity-configuration?view=aspnetcore-6.0&tabs=aspnetcore2x&viewFallbackFrom=aspnetcore-2.1
+builder.Services.Configure<IdentityOptions>(options => {
+   // Default Lockout settings.
+   options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+   options.Lockout.MaxFailedAccessAttempts = 5;
+   options.Lockout.AllowedForNewUsers = true;
+   // Password settings
+   options.Password.RequireDigit = true;
+   options.Password.RequiredLength = 6;
+   options.Password.RequireNonAlphanumeric = false;
+   options.Password.RequireUppercase = false;
+   options.Password.RequireLowercase = true;
+   options.Password.RequiredUniqueChars = 1;
+   // Default User settings.
+   options.User.AllowedUserNameCharacters =
+           "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+   options.User.RequireUniqueEmail = false;
+});
+
+
+// configurar o uso de 'cookies'
+builder.Services.ConfigureApplicationCookie(options => {
+   options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+   options.Cookie.Name = "YourAppCookieName";
+   options.Cookie.HttpOnly = true;
+   options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+   options.LoginPath = "/Identity/Account/Login";
+   // ReturnUrlParameter requires 
+   //using Microsoft.AspNetCore.Authentication.Cookies;
+   options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+   options.SlidingExpiration = true;
+});
+
+
 
 
 builder.Services.AddControllersWithViews();
