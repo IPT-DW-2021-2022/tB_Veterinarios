8c098138fc452763f208ba1f11d4e287d8fa7c3b
jcnpereira <casimiro@ipt.pt>
2022-06-14 16:54:16 +0100

    Endpoint da API:  DonosAPI

--- Vets/Vets/Models/ViewModels.cs
+++ Vets/Vets/Models/ViewModels.cs
@@ -1,6 +1,15 @@
 namespace Vets.Models {
 
    /// <summary>
+   /// ViewModel para ser usado na API dos Donos
+   /// </summary>
+   public class DonosViewModel {
+      public int Id { get; set; }
+      public string Nome { get; set; }
+   }
+
+
+   /// <summary>
    /// Este viewmodel irá recolher os dados dos Animais que a API 
    /// irá apresentar na sua interface
    /// </summary>
